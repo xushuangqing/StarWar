598b1098f0f5eaa87af0b1cb72f11f53fff76008
xushuangqing <thuxushuangqing@gmail.com>
2014-04-12 19:20:49 -0700

    Initial Commit

---
+++ StarWar/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  StarWar
+//
+//  Created by XuShuangqing on 14-4-12.
+//  Copyright XuShuangqing 2014年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ StarWar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StarWar
+//
+//  Created by XuShuangqing on 14-4-12.
+//  Copyright XuShuangqing 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
