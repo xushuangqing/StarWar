bdfa288667b5c7992449d18fbd162feb89645770
xushuangqing <thuxushuangqing@gmail.com>
2014-05-14 10:42:19 +0800

    Add score labels to scoreboard

--- StarWar/SRScoreBoardLayer.mm
+++ StarWar/SRScoreBoardLayer.mm
@@ -7,6 +7,8 @@
 //
 
 #import "SRScoreBoardLayer.h"
+#import "AppDelegate.h"
+#import "User.h"
 
 @implementation SRScoreBoardLayer
 
@@ -21,9 +23,36 @@
 -(id) init
 {
 	if( (self=[super init])) {
-
+        [self dataFetchRequest];
 	}
 	return self;
+}
+
+-(void) dataFetchRequest
+{
+    AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [app managedObjectContext];
+    
+    NSFetchRequest *fetchRequest = [[[NSFetchRequest alloc] init] autorelease];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"User" inManagedObjectContext:context];
+    [fetchRequest setEntity:entity];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"score" ascending:NO];
+    NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    NSError *error;
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+    
+    int i = 0;
+    for (User *info in fetchedObjects){
+        CCLabelTTF *label = [CCLabelTTF labelWithString:[[info score] stringValue] fontName:@"Marker Felt" fontSize:32];
+        [self addChild:label];
+        label.position = ccp(200, 320 - 30*i);
+        i++;
+        if(i>=10)
+            break;
+    }
 }
 
 @end
