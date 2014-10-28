767b47ef6dcd37e44da7504e912f26e9d3c5c2c9
Shuangqing <xushq@hohoyi.com>
2014-10-28 18:57:04 +0800

    Remove scoreboard

--- StarWar/SRMainMenuLayer.mm
+++ StarWar/SRMainMenuLayer.mm
@@ -8,7 +8,6 @@
 
 #import "SRMainMenuLayer.h"
 #import "SRSpaceLayer.h"
-#import "SRScoreBoardLayer.h"
 #import "SRConstants.h"
 #import "SRSpaceScene.h"
 
@@ -35,15 +34,7 @@
     CCMenuItem *startButton = [CCMenuItemImage itemWithNormalImage:@"buttonPlay@2x.png" selectedImage:@"buttonPlay@2x.png" target:self selector:@selector(startButtonPressed:)];
     startButton.position = ccp([UIScreen mainScreen].bounds.size.height/2, 210);
     
-    CCMenuItem *scoreBoardButton = [CCMenuItemImage itemWithNormalImage:@"buttonHighScores@2x.png" selectedImage:@"buttonHighScores@2x.png" target:self selector:@selector(scoreBoardButtonPressed:)];
-    scoreBoardButton.scale = 0.8;
-    scoreBoardButton.position = ccp([UIScreen mainScreen].bounds.size.height/5, 90);
-    
-    CCMenuItem *helpButton = [CCMenuItemImage itemWithNormalImage:@"buttonHelp@2x.png" selectedImage:@"buttonHelp@2x.png" target:self selector:@selector(scoreBoardButtonPressed:)];
-    helpButton.scale = 0.8;
-    helpButton.position = ccp([UIScreen mainScreen].bounds.size.height/5*4, 90);
-    
-    CCMenu* menu = [CCMenu menuWithItems:startButton, scoreBoardButton, helpButton, nil];
+    CCMenu* menu = [CCMenu menuWithItems:startButton, nil];
     menu.position = CGPointZero;
     [self addChild:menu z:zMenu tag:kTagMenu];
 }
@@ -53,13 +44,6 @@
     [self buttonPressed:sender];
     NSLog(@"startButtonPressed");
     [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[SRSpaceScene node]]];
-}
-
-- (void)scoreBoardButtonPressed:(id)sender
-{
-    [self buttonPressed:sender];
-    NSLog(@"scoreBoardButtonPressed");
-    [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[SRScoreBoardLayer scene] ]];
 }
 
 
